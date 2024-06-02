f4ba88123715ecafc0e120b07161906df50e1b7e
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-02 17:07:44 -0500

    Finished problem 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    s = ''
+    for (let i = 1; i <= num; i++){
+        s += string;
+    }
 
+    if (num < 0)
+        s = 'ERROR';
+
+    return s;
 };
 
 // Do not edit below this line
