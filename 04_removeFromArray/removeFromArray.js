f60d79f8af2795192de23e36c6204b5cbb920e05
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-03 15:11:40 -0500

    finished problem 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr) {
+
+    for (let arg = 1; arg < arguments.length; arg++) {
+        let index = 0;
+        while (index < arr.length) {
+            while (arr[index] === arguments[arg]) {
+                arr.splice(index, 1);
+            }
+            index++;
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
