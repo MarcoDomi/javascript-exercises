3a6b2b9b8cf924b6ffe20bc909b2e857393f8d56
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-06-03 16:22:03 -0500

    finished problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+    if ((start < 0 || end < 0) ||
+        (typeof start !== 'number' || typeof end !== 'number')) {
+        return 'ERROR';
+    }
 
+    if (start > end) {
+        let temp = start; 
+        start = end;
+        end = temp;
+    }
+   
+    let total = 0;
+    for (let i = start; i <= end; i++){
+        total += i;
+    }
+
+    return total;
 };
 
 // Do not edit below this line
