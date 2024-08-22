a4089d4df2c5ea05a8cbd869f8a387e06b525e79
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-22 16:58:21 -0500

    finish 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((total, curr) =>{return total + curr;},0);
 };
 
-const multiply = function() {
-
+const multiply = function (arr) {
+  return arr.reduce((total, curr) => { return total * curr }, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exp) {
+  let result = 1;
+  for (let i = 0; i < exp; i++)
+    result *= base;
+
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (fact) {
+  let result = 1;
+  for (let i = fact; i >= 1; i--)
+    result *= i;
+
+  return result;
 };
 
 // Do not edit below this line
