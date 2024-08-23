821e36022ad5c0503fd9ccbf5ccd9110d60479a5
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-23 17:51:37 -0500

    finished 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let oldestPerson = people.reduce((oldest, person) => { 
+        if (!('yearOfDeath' in oldest))
+            oldest['yearOfDeath'] = (new Date()).getFullYear();
+        
+        if (!('yearOfDeath' in person))
+            person['yearOfDeath'] = (new Date()).getFullYear();
 
+        let oldestAge = oldest.yearOfDeath - oldest.yearOfBirth;
+        let personAge = person.yearOfDeath - person.yearOfBirth;
+
+        if (oldestAge < personAge)
+            oldest = structuredClone(person);
+
+        return oldest;
+        
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
