6b941d01de30831a925bcdfd9c9cb1785afbfef6
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-23 14:40:25 -0500

    finished 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let cleanList = str.toLowerCase().split('').filter(ch => (ch.charCodeAt() >= 48 && ch.charCodeAt() <= 57) ||
+                                                             (ch.charCodeAt() >= 65 && ch.charCodeAt() <= 90) ||
+                                                             (ch.charCodeAt() >= 97 && ch.charCodeAt() <= 122));
+    let new_str = cleanList.join('');
+    let reverseStr = cleanList.reverse().join('');
 
+    return new_str === reverseStr;
 };
 
 // Do not edit below this line
