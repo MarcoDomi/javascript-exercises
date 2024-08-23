2b0621af7143752691b9f70c57c1ea4fa6a5fc9b
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-23 15:35:58 -0500

    finished 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    num = Number(num);
+    if (num == 0 || num == 1)
+        return num;
+
+    if (num < 0)
+        return "OOPS";
+
+    let arr = [1, 1];
+    for (let i = 2; i < num; i++)
+        arr.push(arr[i-1] + arr[i-2]);
+    
+    return arr[arr.length-1];
 
 };
 
