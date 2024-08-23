53be7e5e1dcc52621bd6659a93826837c616b4fa
Marco Dominguez <47532404+MarcoDomi@users.noreply.github.com>
2024-08-23 16:19:27 -0500

    finish 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,8 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    return books.reduce((titleList, curr) => {
+        titleList.push(curr.title);
+        return titleList
+    }, []);
 };
 
 // Do not edit below this line
